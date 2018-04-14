6619fe923ca93f15be3bc37953530685882fd692
laurencantlin <laurenrose5321@gmail.com>
2018-04-14 02:00:32 -0400

    cleans up code

--- ValidParenthesis.js
+++ ValidParenthesis.js
@@ -5,7 +5,6 @@ function validParentheses(parens){
             for(var i=0;i<(parens.length);i++){
              if(newStr.length===0){isValid=true; break;}
               newStr=newStr.split("()").join("")
-    //         console.log(i,parens.length,"at length:"+newStr.length, "parens="+newStr)
             }
         }
       return isValid
