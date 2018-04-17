3615fe5f4b5439c25be56b140afa740a70ea7771
laurencantlin <laurenrose5321@gmail.com>
2018-04-17 16:31:36 -0400

    pseudocodes more of solution. cleans up prior code to be more dry

--- SupermarketQueue.js
+++ SupermarketQueue.js
@@ -1,11 +1,11 @@
 //thread pool practice
 function queueTime(customers, n) {
-    if (n=1){
-       var totaltime=0
-       for(var i =0;i<customers.length; i++)  {
-           totaltime=totaltime+customers[i];
-       }
-       return totaltime;
-   
-     }
-   }
+    //if array length=0, time =0
+    if (n = 1) {
+        return customers.reduce((total, num) => { return total + num });
+
+    }
+    //else if n>length of array, then return sum of items in array
+    //else if 1<n<length of of array,??
+
+}
