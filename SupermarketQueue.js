5fa9a036ad7ba933c3e225b3ff4ee4ca436fc50a
laurencantlin <laurenrose5321@gmail.com>
2018-04-17 16:42:29 -0400

    adds another part of queue solution

--- SupermarketQueue.js
+++ SupermarketQueue.js
@@ -1,11 +1,14 @@
 //thread pool practice
 function queueTime(customers, n) {
     if(customers.length===0){return 0}
-    else if (n = 1) {
+    else if (n === 1) {
         return customers.reduce((total, num) => { return total + num });
 
     }
-    //else if n>length of array, then return sum of items in array
-    //else if 1<n<length of of array,??
-
+    else if (n>customers.length){
+        return Math.max(...customers)
+    }
+    else if (1<n<customers.length){
+        console.log("thread pool")
+    }   
 }
